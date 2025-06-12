3d5deaa551976aef2d21d0f311d8a3f72bae787e
Aaron Zachariah <aaron.zachariah@gmail.com>
2025-06-12 19:50:23 -0400

    add metadata and other info

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { ViteSitemap } from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,10 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    ViteSitemap({
+      hostname: 'https://zachaa2.github.io',
+      // list all routes to be in the sitemap
+      routes: ['/', '/resume']
+    })
   ],
 })
